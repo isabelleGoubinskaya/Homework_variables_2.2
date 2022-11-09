98a8df0633194c17f5fbf2da804ac790982dadd9
Isabelle <isagoubinskaya@gmail.com>
2022-11-09 17:21:15 +0100

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        byte london =  47;
+        short madrid = 852;
+        int paris = 2590000;
+        long moscow = 1147000000L;
+        float earth = 4.5F;
+        double universe = 9.0000000000000000;
+        System.out.println("Значение переменной london с типом byte равно " + london);
+        System.out.println("Значение переменной madrid с типом short равно " + madrid);
+        System.out.println("Значение переменной paris с типом int равно " + paris);
+        System.out.println("Значение переменной moscow с типом long равно " + moscow);
+        System.out.println("Значение переменной earth с типом float равно " + earth);
+        System.out.println("Значение переменной universe с типом double равно " + universe);
+
+    }
+}
